5ed658d4bd607c37590cc3e229aca078768a96d5
Minke Zhang <minke.zhang@gmail.com>
2023-04-27 02:05:29 -0700

    Clean up project

--- tank.gd
+++ tank.gd
@@ -1,13 +1,13 @@
 extends CharacterBody2D
 
-const MOTION_SPEED = 16 # Pixels/second.
-const FRICTION_FACTOR = .75
+const MOTION_SPEED = 48 # Pixels/second.
 
 func _physics_process(_delta):
 	var motion = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
 	# Make diagonal movement fit isometric tiles.
 	motion.y /= 2
-	velocity += motion.normalized() * MOTION_SPEED
-	# Apply friction.
-	velocity *= FRICTION_FACTOR
+	
+	velocity = motion.normalized() * MOTION_SPEED
+	
 	move_and_slide()
